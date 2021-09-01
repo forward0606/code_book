e41ce6a893df9efdf5919d39cd258e2dd73f7183
Slashie_person_and_his_Computer_Science_life <76461262+Hacker-Davinci@users.noreply.github.com>
2021-09-02 03:23:53 +0800

    Update a_bipartite.cpp

--- Bipartite_Matching_And_Maximum_Flow/a_bipartite.cpp
+++ Bipartite_Matching_And_Maximum_Flow/a_bipartite.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 # define N 5000 // N has to be larger than n+m
-int n, m, k; // n = left set size, m = right set ,¡÷size, k = number of edge
+int n, m, k; // n = left set size, m = right set ,Â¡Ã·size, k = number of edge
 int match[N]; bool used[N]; vector<int> adj[N];
 bool DFS(int x) {
     for(auto u : adj[x]) {
@@ -25,27 +25,13 @@ int Bipartite_match() {
         match_number += DFS(i);
         //cout<<match_number<<endl;
     }
-// and if(match[i] != -1) {i(right set) and ,¡÷match[i](left set) match}
+// and if(match[i] != -1) {i(right set) and ,Â¡Ã·match[i](left set) match}
     return match_number;
 }
 void init() {
     for(int i = 0;i < k;i++) {
         int a, b; scanf("%d%d", &a, &b);
         b = b + n;
-        adj[a].push_back(b); // a in left set, b ,¡÷in right set
-    }
-}
-int main ()
-{
-    scanf("%d%d%d", &n, &m, &k);
-    init();
-    cout<<Bipartite_match()<<endl;
-    for(int i = 1; i <= n; i++){
-        if(match[i]!=-1)
-            cout<<i<<" "<<match[i]-n<<endl;
-    }
-    for(int i = n+1; i <= n+m; i++){
-        if(match[i]!=-1)
-            cout<<match[i]<<" "<<i-n<<endl;
+        adj[a].push_back(b); // a in left set, b ,Â¡Ã·in right set
     }
 }
