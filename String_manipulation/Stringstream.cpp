e35f8a770cb6822d307808040a03024ecc96fc4c
Slashie_person_and_his_Computer_Science_life <76461262+Hacker-Davinci@users.noreply.github.com>
2021-09-26 16:42:23 +0800

    Create Stringstream.cpp

---
+++ String_manipulation/Stringstream.cpp
@@ -0,0 +1,11 @@
+stringstream ss;  
+ss.str("");  
+ss.clear();  
+getline(cin, s);  
+ss<<s;
+ss>>(string); // stoi can only use for 214748367 as max value.
+ss>>(long long int );  // if use ss>>(long long int), it will be really strong.
+                    
+
+
+  
