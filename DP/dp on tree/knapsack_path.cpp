25bef8de6df90fe3a2da69d09f6516f897f16f22
Slashie_person_and_his_Computer_Science_life <76461262+Hacker-Davinci@users.noreply.github.com>
2021-09-29 11:32:27 +0800

    Update and rename dp_path.cpp to knapsack_path.cpp

--- DP/dp on tree/knapsack_path.cpp
+++ DP/dp on tree/knapsack_path.cpp
@@ -1,7 +1,4 @@
-#include <bits/stdc++.h>
-using namespace std;
 typedef long long int lld;
-lld n, m;
 struct bag
 {
     int w, v;// weight and value.
@@ -12,26 +9,18 @@ set<int>  ::  iterator it1, it2;
 int main ()
 {
     cin>>n>>m; // n object and m weigt
-    for(int i = 0; i < n; i++){
-        cin>>o[i].v>>o[i].w;
-    }
-    for(int i = 1; i <= n; i++){
+    for(int i = 0; i < n; i++)
+        cin>>o[i].v>>o[i].w; 
+    // initialize
+    for(int i = 1; i <= n; i++)
         for(int j = 0; j <= m; j++){
-           // cout << dp[i-1][j] << " " << dp[i][j];
             if(o[i-1].w <= j) dp[i][j] = max(dp[i-1][j], dp[i-1][j-o[i-1].w] + o[i-1].v);
             else {
-                //cout << dp[i-1][j] << " " << dp[i][j]<<endl;
                 dp[i][j] = dp[i-1][j];
             }
-
-           // else cout<<"what " << endl;
-
         }
-    }
-
     int rec_pos = m;
     lld tmp = dp[n][rec_pos];
-
     // the i means the line from down to top and means the object which is used if not continue.
     // the below we can draw a DAG to debug
     for(int i = n; i >= 0; i--){
@@ -39,11 +28,7 @@ int main ()
         rec_pos -= o[i].w;
         tmp = dp[i][rec_pos];
         use.insert(i);
-
     }
-
-    cout<<dp[n][m]<<endl;
-
     it1 = use.begin();
     it2 = use.end();
     cout<<use.size()<<endl;
@@ -52,5 +37,4 @@ int main ()
         cout<<*it1<<" ";
         it1++;
     }
-    cout<<endl;
 }
