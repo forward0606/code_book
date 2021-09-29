6be887f4050054d222bf7fea79afdd635febfb40
Slashie_person_and_his_Computer_Science_life <76461262+Hacker-Davinci@users.noreply.github.com>
2021-09-29 11:24:53 +0800

    Add files via upload

---
+++ useful/sizeint128.cpp
@@ -0,0 +1,36 @@
+typedef __int128 lld;
+__int128 read();
+void print(__int128 x);
+int main()
+{
+    lld n;
+    n = read();
+
+    lld x=read();
+    print(x);
+    cout<<'\n';
+    print(x);
+
+}
+__int128 read() {
+    __int128 x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = x * 10 + ch - '0';
+        ch = getchar();
+    }
+    return x * f;
+}
+void print(__int128 x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) print(x / 10);
+    putchar(x % 10 + '0');
+}
+
